283796f2948e056e9c5e72adaf27b00ae70fb6c4
hemal patel <Hemal@hemals-MBP.attlocal.net>
2018-10-24 10:51:31 -0700

    hemal changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Login from './components/login';
+import Employee from './components/employee'
+
 import './App.css';
 
 class App extends Component {
@@ -7,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <Login />
+        <Employee />
       </div>
     );
   }
