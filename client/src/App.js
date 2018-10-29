45cd526a57e903fee7886ceb8b18a0b4e65ee505
t-dinh <tdinh104@gmail.com>
2018-10-29 13:44:12 -0700

    change update

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,12 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import DataTable from './components/dataTable';
 import Navbar from './components/navbar';
-import Contacts from './components/contacts';
+import Contacts from './components/pages/contacts';
 import Bids from './components/bids';
 import Projects from './components/projects';
 import Login from './components/login';
 import Employees from './components/pages/employees';
+import EmployeeUpdateForm from './components/forms/EmployeeUpdateForm';
 import NewEmployeeForm from './components/forms/newEmployeeForm';
 import NewContactForm from './components/forms/newContactForm';
 import NewBidForm from './components/forms/newBidForm';
@@ -32,6 +33,7 @@ class App extends Component {
             <Route path="/dataTable" render={() => <DataTable />} />
             <Route path="/contacts" render={() => <Contacts />}/>
             <Route path="/employee" render={() => <Employees />} />
+            <Route path="/update" render={() => <EmployeeUpdateForm /> } />
             </div>
             </Switch>        
       </div>
