4bf23d48c96ce892997f83a03dcec8033a9ba61d
t-dinh <tdinh104@gmail.com>
2018-10-30 17:03:18 -0700

    delete redux works

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 
 import React, { Component } from 'react';
-// import { Switch, Route } from 'react-router-dom';
-// import Contacts from './components/pages/contacts';
-// import Bids from './components/pages/bids';
-// import Projects from './components/pages/projects'
+import { Switch, Route } from 'react-router-dom';
+import Contacts from './components/pages/contacts';
+import Bids from './components/pages/bids';
+import Projects from './components/pages/projects'
 import Login from './components/login';
 import Employees from './components/pages/employees';
-// import EmployeeUpdateForm from './components/forms/EmployeeUpdateForm';
+import EmployeeUpdateForm from './components/forms/EmployeeUpdateForm';
 // import NewEmployeeForm from './components/forms/newEmployeeForm';
 // import NewContactForm from './components/forms/newContactForm';
 // import NewBidForm from './components/forms/newBidForm';
 // import NewProjectForm from './components/forms/newProjectForm';
 // import Calendar from 'react-calendar';
-// import Navbar from './components/navbar';
+import Navbar from './components/navbar';
 import './App.css';
 
   
@@ -20,13 +20,15 @@ class App extends Component {
     state = {
     username: 'jtorres',
     password: 'fj39g883',
-    loggedin: false
+    loggedin: true
     };
 
   render() {
     return (
       <div className="tablecontainer">
-      {/* <div className = "navbar"><Navbar /></div>
+       {/* {
+        this.state.loggedin && <div className = "navbar"><Navbar /></div>
+        } */}
         <Switch>
             <Route exact path="/" render={() => <Login />} />
             <Route path="/contacts" render={() => <Contacts />}/>
@@ -34,11 +36,12 @@ class App extends Component {
             <Route path="/bids" render={() => <Bids />} />
             <Route path="/projects" render={() => <Projects />} />
             <Route path="/update" render={() => <EmployeeUpdateForm /> } />
+      
             </Switch>    
 
-            <Calendar/>   */}
-            <Login />
-            <Employees />
+            {/* <Calendar/> */}
+            {/* <Login /> */}
+            {/* <Employees /> */}
       </div>
     );
   }
